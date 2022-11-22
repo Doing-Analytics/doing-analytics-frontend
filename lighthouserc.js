4246a9a06eff77515f041438936de5285eab9541
Ted <wodf410@gmail.com>
2022-11-22 23:37:34 +0800

    lighthouse init

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,7 +1,19 @@
 module.exports = {
   ci: {
     collect: {
+      numberOfRuns: 3,
       startServerCommand: "npm run dev",
+    },
+    assert: {
+      assertions: {
+        "first-contentful-paint": ["warn", { maxNumbericValue: 4000 }],
+        "categories:performance": ["warn", { minScore: 0.9 }],
+        "categories:accessibility": ["error", { minScore: 0.9 }],
+        "categories:seo": ["error", { minScore: 1 }],
+        "installable-manifest": "on",
+        "service-worker": "on",
+        "works-offline": "on",
+      },
     },
     upload: {
       target: "temporary-public-storage",
